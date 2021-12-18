3c2bc43cf93390bcfb65de4c87f734d4dd97c644
Denailly <danilocalheirosjr@gmail.com>
2021-12-18 15:00:56 -0300

    att1

---
+++ JS/main.js
@@ -0,0 +1,32 @@
+var idade = 17
+if (idade >= 18) {
+    console.log("pode entrar")
+}
+
+// --- AULA 2 ---
+// --- DICIONÁRIO ---
+// {} SERVE PARA DICIONÁRIOS
+/*var frutas = { nome: "abacaxi", cor: "amarelo" }
+console.log(frutas.nome)
+
+
+// --- ARRAYS ---
+//[] SERVE PARA ARRAYS
+var array = ["Mazda RX7", "Honda 350 Saara", "Mazda Meduza", "Silvia S15"];
+var numeros = [1, 3]
+array.push("Kawazaki Ninja 1100"); //Adiciona na no último slot do Array
+array.pop(); //Limpa o ultimo slot do Array
+
+console.log(array[0]);
+console.log(array);
+console.log("o tamanho do array é: " + array.length)
+console.log(array.toString()[10]) //
+console.log(numeros[1] + numeros[0])
+console.log(array.join(" - "))*/
+
+//--- AULA 1 ---
+/*var nome = "Danilo Calheiros"
+var idade = 17
+alert("Salve salve " + nome + " Você tem " + idade + " Anos");
+console.log("teste");
+console.log(nome.replace("Danilo", "Fusei"));*/
